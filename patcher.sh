056e12f93c4261e582d4928a46c6ad0b2288bbf8
Ilya Lebedev <ashuli2003@gmail.com>
2017-05-26 09:07:38 +0300

    Revert "patcher: quarrion: Don't silent log for now"
    This reverts commit f04b15464257a26c0cb787458e11509f4c0333ac.

--- patcher.sh
+++ patcher.sh
@@ -12,9 +12,12 @@ cd $ROM_TREE/build
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
+cd $ROM_TREE/system/core
+git clean -f -d && git reset --hard
 
 cd $ROM_TREE
 
 ### Custom patches
 patch -d build					-p1 -s -N --no-backup-if-mismatch < $CUSTOM/build-tag.patch
 patch -d packages/apps/LockClock		-p1 -s -N --no-backup-if-mismatch < $CUSTOM/lockclock-wakelock.patch
+patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $CUSTOM/silence_audit_log.patch
